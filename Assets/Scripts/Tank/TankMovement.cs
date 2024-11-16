c830fbd6e7656f11b99030a217bd0e0bcba610f8
stitch <epis201.1@gmail.com>
2024-11-16 18:39:39 -0500

    primer commit

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;
+    public int m_scene = 1;
+    public float m_Speed = 12f;            
+    public float m_TurnSpeed = 180f;       
+    public AudioSource m_MovementAudio;    
+    public AudioClip m_EngineIdling;       
+    public AudioClip m_EngineDriving;      
+    public float m_PitchRange = 0.2f;
+
+    //new
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    private string m_MovementAxisName;     
+    private string m_TurnAxisName;         
+    private Rigidbody m_Rigidbody;         
+    private float m_MovementInputValue;    
+    private float m_TurnInputValue;        
+    private float m_OriginalPitch;
+
+    //NPC
+    public float speed = 2.0f;
+    // Accuracy
+    public float accuracy = 0.01f;
+    // Object target
+    public Transform goal;
+
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable ()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        m_OriginalPitch = m_MovementAudio.pitch;
+
+        if (m_PlayerNumber > 2)
+        {
+            this.transform.LookAt(goal.position);
+        }
+
+        //print("asdasdasd "+m_scene);
+    }
+
+
+    private void Update()
+    {
+        // Store the player's input and make sure the audio for the engine is playing.
+        if (m_PlayerNumber <= 2)
+        {
+            print(m_MovementAxisName);
+            print(m_TurnAxisName);
+            m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+            m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+            EngineAudio();
+        }
+        else if (m_scene == 2) {
+            if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3) currentWP++;
+            if (currentWP >= waypoints.Length) currentWP = 0;
+            Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (m_PlayerNumber > 2 && m_scene != 2)
+        {
+            this.transform.LookAt(goal.position);
+            Vector3 direction = goal.position - this.transform.position;
+            Debug.DrawRay(this.transform.position, direction, Color.red);
+            if (direction.magnitude > accuracy)
+            {
+                this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+
+
+    private void EngineAudio()
+    {
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+        {
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                // ... change the clip to idling and play it.
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play ();
+            }
+        }
+        else
+        {
+            // Otherwise if the tank is moving and if the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                // ... change the clip to driving and play.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Move and turn the tank.
+        Move ();
+        Turn ();
+    }
+
+
+    private void Move()
+    {
+        // Adjust the position of the tank based on the player's input.
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn()
+    {
+        // Adjust the rotation of the tank based on the player's input.
+         // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+    }
+}
